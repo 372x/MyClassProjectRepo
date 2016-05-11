5300c118437fa53d659c2566ceb01e08ec8a58da
372x <js016Z@icloud.com>
2016-05-11 17:16:47 -0600

    adding css to todo

--- client/App.jsx
+++ client/App.jsx
@@ -5,7 +5,23 @@ import todos from 'pages/todo';  // todos is what was app on the todo.js file
 import project from 'pages/project';
 
 $(function(){
-  todos.init();
+	// what page are we on?
+  var url = window.location.pathname;
+
+    // our first javascript router; better syntax than if statement;
+  switch (url){   // switch statement; going to look for lots of values of 'url'; executes same as if statement, but better syntax, better-looking code
+    case '/pages/todo.html':  // takes place of if statement
+      todos.init();
+		break;
+    case '/pages/project.html':
+        // init the project javascript
+		break;
+  }
+
+	// if (url === 'pages/todo.html'){
+	//	todos.init();
+	// }
+  // todos.init();
 });
 
 // this file manages all the pages of our website
--- public/application.js
+++ public/application.js
@@ -65,7 +65,24 @@
 	var _pagesProject2 = _interopRequireDefault(_pagesProject);
 	
 	(0, _jquery2['default'])(function () {
-	  _pagesTodo2['default'].init();
+	  // what page are we on?
+	  var url = window.location.pathname;
+	
+	  // our first javascript router; better syntax than if statement;
+	  switch (url) {// switch statement; going to look for lots of values of 'url'; executes same as if statement, but better syntax, better-looking code
+	    case '/pages/todo.html':
+	      // takes place of if statement
+	      _pagesTodo2['default'].init();
+	      break;
+	    case '/pages/project.html':
+	      // init the project javascript
+	      break;
+	  }
+	
+	  // if (url === 'pages/todo.html'){
+	  //	todos.init();
+	  // }
+	  // todos.init();
 	});
 	
 	// this file manages all the pages of our website
